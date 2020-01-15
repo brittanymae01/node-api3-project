f2e0f98fe109829636781c99abcd83900a01048a
brittanymae01 <brittanymae01>
2020-01-15 13:50:47 -0800

    set up route methods in postRouter.js

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,78 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const Posts = require("./postDb.js");
+
+router.get("/", (req, res) => {
   // do your magic!
+  Posts.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        errorMessage: "problem retreiving posts"
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
+
+  Posts.getById(req.params.id)
+    .then(post => {
+      if (post) {
+        return res.status(200).json(post);
+      } else {
+        return res.status(404).json({
+          errorMessage: "post by the provided ID does not exist"
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      return res.status(500).json({
+        errorMessage: "error reteiving post"
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
+  Posts.remove(req.params.id)
+    .then(post => {
+      if (!post) {
+        return res.status(404).json({
+          errorMessage: "post by the provided Id does not exist"
+        });
+      } else {
+        return res.status(200).json(post);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      return res.status(500).json({
+        errorMessage: "error removing post"
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
+  Posts.update(req.params.id, req.body)
+    .then(updated => {
+      if (!updated) {
+        res.status(404).json({ message: "Could not edit, does not exist!" });
+      } else {
+        res.status(200).json(updated);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Post does not exist!" });
+    });
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
+
+server.use(express.json());
+
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
